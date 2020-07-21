66791a95a0c58f8ed5ac48e404827a771425f751
areed145 <areed145@gmail.com>
2020-07-20 23:59:10 -0500

    move out of try

--- src/upload_weather_data.py
+++ src/upload_weather_data.py
@@ -156,10 +156,11 @@ try:
         print("No new data since {0}.".format(end_time_last_slice))
 
     # if register_dataset:
-    ds = Dataset.Tabular.from_delimited_files(
-        dstor.path("{}/**/*.csv".format(args.ds_name)),
-        partition_format="/{partition_date:yyyy/MM/dd/HH/mm/ss}/data.csv",
-    )
-    ds.register(ws, name=args.ds_name, create_new_version=True)
 except Exception:
     pass
+
+ds = Dataset.Tabular.from_delimited_files(
+    dstor.path("{}/**/*.csv".format(args.ds_name)),
+    partition_format="/{partition_date:yyyy/MM/dd/HH/mm/ss}/data.csv",
+)
+ds.register(ws, name=args.ds_name, create_new_version=True)
